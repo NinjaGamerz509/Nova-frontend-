275c3bfabebd717095a628ed648774a9e1e65297
NinjaGamerz509 <srahemat943@gmail.com>
2026-05-26 15:27:40 +0530

    fix ip address

--- app/src/main/java/com/ninja/nova/utils/Constants.kt
+++ app/src/main/java/com/ninja/nova/utils/Constants.kt
@@ -1,11 +1,10 @@
 package com.ninja.nova.utils
 
 object Constants {
-    const val BASE_URL = "http://10.0.2.2:3000/" // emulator
-    // For real device use your local IP: "http://192.168.x.x:3000/"
+    const val BASE_URL = "http://192.0.0.2:3000/"
     const val PREF_TOKEN = "nova_token"
     const val PREF_SESSION = "nova_session"
-    const val SOCKET_URL = "http://10.0.2.2:3000"
+    const val SOCKET_URL = "http://192.0.0.2:3000"
     const val WAKE_WORD = "Hey Nova"
     const val LISTEN_TIMEOUT_MS = 5000L
 }
