0b47e5dcb26990c3dd149ba30d7222dbfb303adb
NinjaGamerz509 <srahemat943@gmail.com>
2026-05-27 12:41:53 +0530

    fix build errors

--- app/src/main/java/com/ninja/nova/ui/components/FinanceWindow.kt
+++ app/src/main/java/com/ninja/nova/ui/components/FinanceWindow.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
+import androidx.compose.foundation.clickable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
